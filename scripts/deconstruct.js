5e9cc69f1e8c5575a58dd33562bff4e62b81b09b
Angelo Tomarcafe <angelo.tomarcafe@gmail.com>
2015-06-19 15:02:49 +0600

    removed digits from filenames

--- scripts/deconstruct.js
+++ scripts/deconstruct.js
@@ -4,17 +4,11 @@ var path = require('path');
 var chaingear = require("../chaingear.json");
 console.log("detected " +  chaingear.length + " entries in chaingear.json");
 
-function paddy(val, padlen, padchar) { //http://stackoverflow.com/questions/1267283/how-can-i-create-a-zerofilled-value-using-javascript
-  var pad_char = typeof padchar !== 'undefined' ? padchar : '0';
-  var pad = new Array(1 + padlen).join(pad_char);
-  return (pad + val).slice(-pad.length);
-}
-
 function saveSystem(system, index){
   if (!system.name) {
     throw new Error("system must have name")
   }
-  var filename = path.join(__dirname,"..","sources.json",(paddy(index, 4)+"-" + system.name + ".json"));
+  var filename = path.join(__dirname,"..","sources.json",system.name + ".json");
   fs.writeFileSync(filename,  JSON.stringify(system, null, 4));
 }
 
