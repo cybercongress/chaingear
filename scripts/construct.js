63b8ed99d7f9ac27fdc9139e2f0eaacc5fdfe298
Dima Starodubcev <d@cyber.fund>
2015-10-02 17:05:21 +0100

    Add stub for chaingear quality report

--- scripts/construct.js
+++ scripts/construct.js
@@ -51,7 +51,29 @@ function act() {
 
   fs.writeFileSync(path.join(__dirname, "..", "chaingear.json"), JSON.stringify(result, null, 4));
   fs.writeFileSync(path.join(__dirname, "..", "v1.json"), JSON.stringify(result, null, 4));
-  console.log("combined " + result.length + " entries into chaingear.json");
+  console.log("");
+  console.log("Combined " + result.length + " entries into v1.json");
+  console.log("");
+  console.log("Without logo:");
+  console.log("Wihtou headline: ");
+  console.log("");
+  console.log("Cryptocurrencies");
+  console.log("No Genesis ID:");
+  console.log("No Token name:");
+  console.log("No Ticker:");
+  console.log("No Code link:");
+  console.log("No Explorer link:");
+  console.log("No Wallet link:");
+  console.log("No Paper link:");
+  console.log("No Exchange link:");
+  console.log("");
+  console.log("Cryptoassets");
+  console.log("No Genesis ID:");
+  console.log("No Token name:");
+  console.log("No Ticker:");
+  console.log("");
+  console.log("Projects count:");
+  console.log("");
 }
 
 act();
