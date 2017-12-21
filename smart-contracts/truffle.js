573449b73bec9046019758448898975d967ead09
Alex Bazhanau <bazhanau@outlook.com>
2017-12-21 16:53:35 +0300

    truffle.js updared

--- smart-contracts/truffle.js
+++ smart-contracts/truffle.js
@@ -4,6 +4,19 @@ module.exports = {
       host: "localhost",
       port: 8545,
       network_id: "*" // Match any network id
+    },
+    kovan: {
+      host: "localhost",
+      port: 8545,
+      network_id: 42,
+      gasPrice: 1000000000
+    },
+    live: {
+      host: "localhost",
+      port: 8545,
+      network_id: 1,
+      gasPrice: 10000000000,
+      from: "0x00b2266565a2dF4dF0Dd473281b7bB88A86b27dd"
     }
   }
-};
+}
