3786ea99299a5c06e9c1a8755f09c9e91f329e05
Andrey <luzkovskyandrey@gmail.com>
2018-03-27 16:18:29 +0300

    try use ipfs direct withour orbit, add field in smart contract

--- app/src/containers/test/index.js
+++ app/src/containers/test/index.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
 
-const IPFS = require('ipfs')
-const OrbitDB = require('orbit-db')
+// const IPFS = require('ipfs')
+// const OrbitDB = require('orbit-db')
 
-const ipfsOptions = {
-  EXPERIMENTAL: {
-    pubsub: true
-  },
-}
+// const ipfsOptions = {
+//   EXPERIMENTAL: {
+//     pubsub: true
+//   },
+// }
+
+const IPFS = require('ipfs-api');
+const ipfs = new IPFS({ host: 'ipfs.infura.io', port: 5001, protocol: 'https' });
+
 
 class Test extends Component {
   constructor(props) {
@@ -17,34 +21,48 @@ class Test extends Component {
   }
 
   get() {
-    alert('get');
+    //alert('get');
+    const hash = 'QmRoRQ9E6qqP8GKSuMZzFuimYqrk6d6S8uLVB3EtTGg4t7';
+    ipfs.get(hash, function (err, files) {
+      const buf = files[0].content;
+      var temp = JSON.parse(buf.toString());
+      debugger
+    });
   }
 
   set() {
- debugger
-    const ipfs = new IPFS(ipfsOptions);
-    const orbitdb = new OrbitDB(ipfs)
-debugger
-ipfs.on('error', (e) => {
-  console.log(e);
-  debugger
-})
-    ipfs.on('ready', async () => {
-      // debugger
+    const abi = {
+      name: 'test'
+    };
+    const buffer = Buffer.from(JSON.stringify(abi));
 
-  const access = {
-    // Give write access to ourselves
-    write: [orbitdb.key.getPublic('hex')],
-  }
-debugger
-      const db = await orbitdb.keyvalue('chaingear.abis', access)
+    ipfs.add(buffer, (err, ipfsHash) => {
       debugger
-      console.log(db.address.toString())
-        // .then(db => db.put('test', { name: 'hello' }))
-        // .then(() => {
-        //   debugger
-        // })
-    });
+    })
+//  debugger
+//     const ipfs = new IPFS(ipfsOptions);
+//     const orbitdb = new OrbitDB(ipfs)
+// debugger
+// ipfs.on('error', (e) => {
+//   console.log(e);
+//   debugger
+// })
+//     ipfs.on('ready', async () => {
+//       // debugger
+
+//   const access = {
+//     // Give write access to ourselves
+//     write: [orbitdb.key.getPublic('hex')],
+//   }
+// debugger
+//       const db = await orbitdb.keyvalue('chaingear.abis', access)
+//       debugger
+//       console.log(db.address.toString())
+//         // .then(db => db.put('test', { name: 'hello' }))
+//         // .then(() => {
+//         //   debugger
+//         // })
+//     });
   }
 
   render() {
--- smart-contracts/contracts/Chaingear.sol
+++ smart-contracts/contracts/Chaingear.sol
@@ -4,17 +4,19 @@ contract Chaingear  {
   struct ContractItem {
     string name;
     address contractAddress;
+    string ipfsHash;
   }
 
   ContractItem[] public contracts;
   event EntryCreated(address owner, uint entryId);
 
-  function register(string _name, address _address) external {
+  function register(string _name, address _address, string _ipfsHash) external {
 
         contracts.push(ContractItem(
         {
             name: _name,
-            contractAddress: _address
+            contractAddress: _address,
+            ipfsHash: _ipfsHash
         }));
     
         EntryCreated(msg.sender, contracts.length - 1);
