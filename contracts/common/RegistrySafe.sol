49dc0c9e916bbe0f03e9ba28e096fc4070228bbb
Valery Litvin <litvintech@gmail.com>
2018-04-13 19:08:13 +0300

    Added drafted Safe contract, refactored IPFSeable

--- contracts/common/RegistrySafe.sol
+++ contracts/common/RegistrySafe.sol
@@ -3,10 +3,10 @@ pragma solidity ^0.4.18;
 import "zeppelin-solidity/contracts/ownership/Ownable.sol";
 import "zeppelin-solidity/contracts/lifecycle/Destructible.sol";
 
-/* destroy? */
-contract RegistrySafe is Ownable {
 
-    function() public payable { }
+contract RegistrySafe is Ownable, Destructible {
+
+    function RegistrySafe() public payable { }
 
     function claim(address _entryOwner, uint _amount)
         public
