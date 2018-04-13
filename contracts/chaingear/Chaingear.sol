0f1770dd8aef0fe0f514aca363faaadb76a8f575
Valery Litvin <litvintech@gmail.com>
2018-04-13 18:46:33 +0300

    Added unstable, non-tested, drafted version of cross integration between chaingear/Rs

--- contracts/chaingear/Chaingear.sol
+++ contracts/chaingear/Chaingear.sol
@@ -4,8 +4,9 @@ import "zeppelin-solidity/contracts/math/SafeMath.sol";
 import "zeppelin-solidity/contracts/AddressUtils.sol";
 import "zeppelin-solidity/contracts/token/ERC721/ERC721Token.sol";
 import "../common/SplitPaymentChangeable.sol";
-import "../common/IPFSeable.sol";
+import "./IPFSeable.sol";
 import "./RegistryBase.sol";
+import "../common/ChaingearRegistrable.sol";
 
 
 contract Chaingear is RegistryBase, ERC721Token, IPFSeable, SplitPaymentChangeable {
@@ -43,17 +44,22 @@ contract Chaingear is RegistryBase, ERC721Token, IPFSeable, SplitPaymentChangeab
         returns (uint256 registryId)
     {
         require(msg.value == registryRegistrationFee_);
-        // require checkInterface(_registryAddress)
-        // require setRegisteredInChaingear (-> CANNOT DELETE/TRANSFER LOCAL)
+
+        ChaingearRegistrable registryContract = ChaingearRegistrable(_registryAddress);
+        require(registryContract.implementsChaingearedRegistry() == true);
+        require(registryContract.setChaingearMode(msg.sender, true) == true);
+
+        bytes32 contractChaingearVersion = registryContract.getChaingeareableVersion();
 
         Registry memory registry = (Registry(
         {
             name: _registryName,
             contractAddress: _registryAddress,
-            creator: msg.sender, //TODO check throw interface
+            creator: msg.sender,
             linkABI: _linkABI,
             registrationTimestamp: block.timestamp,
-            owner: msg.sender
+            owner: msg.sender,
+            chaingeareableVersion: contractChaingearVersion
         }));
 
         uint256 newRegistryId = registries.push(registry) - 1;
@@ -69,8 +75,11 @@ contract Chaingear is RegistryBase, ERC721Token, IPFSeable, SplitPaymentChangeab
         whenNotPaused
     {
         require (ERC721BasicToken.ownerOf(_registryID) == msg.sender);
-        // require checkValidState(_registryAddress)
-        // require unsetRegisteredInChaingear(-> CAN DELETE/TRANSFER LOCAL)
+        address registryAddress = registries[_registryID].contractAddress;
+
+        ChaingearRegistrable registryContract = ChaingearRegistrable(registryAddress);
+        require(registryContract.owner() == msg.sender);
+        require(registryContract.setChaingearMode(msg.sender, false) == false);
 
         string storage registryName = registries[_registryID].name;
         uint256 registryIndex = allTokensIndex[_registryID];
@@ -84,7 +93,7 @@ contract Chaingear is RegistryBase, ERC721Token, IPFSeable, SplitPaymentChangeab
         super._burn(msg.sender, _registryID);
 
         //rethink this event
-         RegistryUnregistered(msg.sender, registryName);
+        RegistryUnregistered(msg.sender, registryName);
     }
 
     function updateRegistryOwnership(uint256 _registryID, address _newOwner)
@@ -92,14 +101,21 @@ contract Chaingear is RegistryBase, ERC721Token, IPFSeable, SplitPaymentChangeab
         whenNotPaused
     {
         require (ERC721BasicToken.ownerOf(_registryID) == msg.sender);
-        // require checkValidState(_registryAddress)
-        // require unsetRegisteredInChaingear
-        // check whitelist/permissioning
+
+        address registryAddress = registries[_registryID].contractAddress;
+        ChaingearRegistrable registryContract = ChaingearRegistrable(registryAddress);
+
+        require(registryContract.owner() == msg.sender);
+        require(registryContract.transferTokenizedOnwerhip(msg.sender, _newOwner) == true);
+        require(registryContract.owner() == _newOwner);
+
         string storage registryName = registries[_registryID].name;
+        registries[_registryID].owner = _newOwner;
+
         super.removeTokenFrom(msg.sender, _registryID);
         super.addTokenTo(_newOwner, _registryID);
 
-         RegistryTransferred(msg.sender, registryName, _newOwner);
+        RegistryTransferred(msg.sender, registryName, _newOwner);
     }
 
     function registryRegistrationFee()
